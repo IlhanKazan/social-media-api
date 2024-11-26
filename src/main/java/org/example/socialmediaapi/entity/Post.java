46f33e8f71cbdb7cb33efba058aa5dfc588412a1
Ilhan Kazan <ilhan.kazan3664@gmail.com>
2024-11-27 00:03:04 +0300

    Unused command lines removed

--- src/main/java/org/example/socialmediaapi/entity/Post.java
+++ src/main/java/org/example/socialmediaapi/entity/Post.java
@@ -10,9 +10,7 @@ import java.util.Date;
 @AllArgsConstructor
 @ToString
 @Entity
-@Table(name = "POSTS",  uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"FLIGHT", "NUMBER"})
-})
+@Table(name = "POSTS")
 public class Post {
 
     @Id
