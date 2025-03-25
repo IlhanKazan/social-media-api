50080c5e7678c551bf43bbb2f9415d33e03bdedb
Ilhan Kazan <ilhan.kazan3664@gmail.com>
2025-03-25 06:09:43 +0300

    deploy developments v8

--- src/main/java/org/example/socialmediaapi/mappers/AccountMapper.java
+++ src/main/java/org/example/socialmediaapi/mappers/AccountMapper.java
@@ -12,27 +12,27 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
     Account requestToAccount(AccountRequest accountRequest);
-    /*@Mapping(source = "username", target = "username")
+    @Mapping(source = "username", target = "username")
     @Mapping(source = "email", target = "email")
     @Mapping(source = "phone", target = "phone")
-    @Mapping(source = "interactions", target = "interactions")*/
+    @Mapping(source = "interactions", target = "interactions")
     AccountResponse accountToResponse(Account account);
     Account responseToAccount(AccountResponse accountResponse);
     List<AccountResponse> accountsToResponses(List<Account> accounts);
 
-    /*@Mapping(source = "username", target = "username")
+    @Mapping(source = "username", target = "username")
     @Mapping(source = "posts", target = "posts")
-    @Mapping(source = "interactions", target = "interactions")*/
+    @Mapping(source = "interactions", target = "interactions")
     WebAccountResponse accountToWebAccountResponse(Account account);
     List<WebAccountResponse> accountsToWebAccountResponses(List<Account> accounts);
 
-    /*@Mapping(source = "username", target = "username")
+    @Mapping(source = "username", target = "username")
     @Mapping(source = "password", target = "password")
     @Mapping(source = "email", target = "email")
     @Mapping(source = "phone", target = "phone")
     @Mapping(source = "posts", target = "posts")
     @Mapping(source = "interactions", target = "interactions")
-    @Mapping(source = "role", target = "role")*/
+    @Mapping(source = "role", target = "role")
     AdminAccountResponse accountToAdminAccountResponse(Account account);
     List<AdminAccountResponse> accountsToAdminAccountResponses(List<Account> accounts);
     AccountResponse adminAccountResponseToAccountResponse(AdminAccountResponse adminAccountResponse);
