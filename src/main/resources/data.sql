2bf90eab5a4b73cca1ec528d09bd7be70b7bdea1
Ilhan Kazan <ilhan.kazan3664@gmail.com>
2025-03-25 06:46:41 +0300

    deploy developments v11

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,7 @@ INSERT INTO public.ACCOUNTS (username, password, email, phone, status, createDat
                                                                                                                      ('cem', '$2a$10$ngCz2oIlDJF9qVT5LE09LuyswA6AkD.dEBujtr9i.ctr899zENgOG', 'cem_85638@gmail.com', '05555555556', DEFAULT, NOW(), NULL, 2),
                                                                                                                      ('deniz', '$2a$10$8JB8gp41d.wCkAGNyOxVgOHg1G5VVkl2pFzbf02X8L/OFjATv8.vu', 'deniz_1298@gmail.com', '05555555557', DEFAULT, NOW(), NULL, 2),
                                                                                                                      ('ahmet', '$2a$10$rKCVXxAZ4PxY8QGbOuUrBuJU96rdA20EaqmtFaEDyzVUdKcp9j0Yq', 'ahmet123@gmail.com', '05555555558', DEFAULT, NOW(), NULL, 2),
-                                                                                                                     ('furkan', '$2a$10$57ZZlufwwTsl4rX4gCo8rOr203KibczuahXCW5uDswZ3TVaxPTGZa', 'deniz_1298@gmail.com', '05555555557', DEFAULT, NOW(), NULL, 2);
+                                                                                                                     ('furkan', '$2a$10$57ZZlufwwTsl4rX4gCo8rOr203KibczuahXCW5uDswZ3TVaxPTGZa', 'furkan3875@gmail.com', '01234567890', DEFAULT, NOW(), NULL, 2);
 
 -- POSTS tablosuna veri ekleme
 INSERT INTO public.POSTS (accountId, context, status, createDate, updateDate) VALUES
