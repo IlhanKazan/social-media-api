43de93eb95fe50a43384680e75c4ffe55264e2f3
Ilhan Kazan <ilhan.kazan3664@gmail.com>
2025-03-25 06:39:01 +0300

    deploy developments v10

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,14 +8,16 @@ INSERT INTO public.ACCOUNTS (username, password, email, phone, status, createDat
                                                                                                                      ('ilhan', '$2a$10$fGO.odXMzmReI5kNUPkW4e.z.MYEn/YKcM1g2ORzeoPPZjjH.H0Xi', 'ilhan.5453@gmail.com', '05555555555', DEFAULT, NOW(), NULL, 1),
                                                                                                                      ('cem', '$2a$10$ngCz2oIlDJF9qVT5LE09LuyswA6AkD.dEBujtr9i.ctr899zENgOG', 'cem_85638@gmail.com', '05555555556', DEFAULT, NOW(), NULL, 2),
                                                                                                                      ('deniz', '$2a$10$8JB8gp41d.wCkAGNyOxVgOHg1G5VVkl2pFzbf02X8L/OFjATv8.vu', 'deniz_1298@gmail.com', '05555555557', DEFAULT, NOW(), NULL, 2),
-                                                                                                                     ('ahmet', '$2a$10$rKCVXxAZ4PxY8QGbOuUrBuJU96rdA20EaqmtFaEDyzVUdKcp9j0Yq', 'ahmet123@gmail.com', '05555555558', DEFAULT, NOW(), NULL, 2);
+                                                                                                                     ('ahmet', '$2a$10$rKCVXxAZ4PxY8QGbOuUrBuJU96rdA20EaqmtFaEDyzVUdKcp9j0Yq', 'ahmet123@gmail.com', '05555555558', DEFAULT, NOW(), NULL, 2),
+                                                                                                                     ('furkan', '$2a$10$57ZZlufwwTsl4rX4gCo8rOr203KibczuahXCW5uDswZ3TVaxPTGZa', 'deniz_1298@gmail.com', '05555555557', DEFAULT, NOW(), NULL, 2);
 
 -- POSTS tablosuna veri ekleme
 INSERT INTO public.POSTS (accountId, context, status, createDate, updateDate) VALUES
                                                                                             (1, 'Merhaba Dünya!', DEFAULT, NOW(), NULL),
-                                                                                            (3, 'Merhaba Ilhan!', DEFAULT, NOW(), NULL),
+                                                                                            (3, 'Merhaba la gardas ?', DEFAULT, NOW(), NULL),
                                                                                             (4, 'Bugun hava cok bozdu!', DEFAULT, NOW(), NULL),
-                                                                                            (2, 'Hoca imza almaya basladi mi?', DEFAULT, NOW(), NULL);
+                                                                                            (2, 'Hoca imza almaya basladi mi?', DEFAULT, NOW(), NULL),
+                                                                                            (5, 'Naber müdür ?', DEFAULT, NOW(), NULL);
 
 -- INTERACTIONS tablosuna veri ekleme
 INSERT INTO public.INTERACTIONS (accountId, postId, context, type, status, createDate, updateDate) VALUES
